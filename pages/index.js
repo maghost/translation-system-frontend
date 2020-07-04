3d4158e0d4958cd583948206c2a9811e355f9e7e
Matheus Maghost <matheus@gamersclub.com.br>
2020-07-04 12:55:04 -0300

    feat: dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from "next/head";
+import Link from "next/link";
 
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.scss";
 import { getSortedPostsData } from "../lib/posts";
+
+import Date from "../components/date";
+import Layout, { siteTitle } from "../components/layout";
+
+import utilStyles from "../styles/utils.module.scss";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -20,6 +24,7 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
+
       <section className={utilStyles.headingMd}>
         <p>
           Hello, I'm <b>Matheus</b>. I'm a software enginer and a game lover.
@@ -29,21 +34,27 @@ export default function Home({ allPostsData }) {
           </a>
           .
         </p>
+
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
+
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
